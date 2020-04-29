0824dd3a5a9258a66bff1f1718608f76b827b0b4
Hatim <hatimd80@gmail.com>
2020-04-29 14:11:58 +0530

    Code from Rippenapps

--- GoGreen/Supporting Classes/APIConstant.swift
+++ GoGreen/Supporting Classes/APIConstant.swift
@@ -9,16 +9,14 @@
 import Foundation
 import UIKit
 
-
-let baseApiUrl = "http://13.126.37.218/gogreen/index.php" //"http://52.66.242.183/gogreen/index.php/"  //"http://13.126.37.218/gogreen/index.php/" // production http://13.126.37.218/gogreen/index.php
+//let baseApiUrl = "http://13.126.37.218/gogreen/index.php/" // production
+let baseApiUrl = "http://52.66.242.183/gogreen/index.php/" // new url
 //let baseApiUrl =  "http://13.126.99.14/gogreen/index.php/"  // dev
 //let baseApiUrl =  "http://13.126.99.14/gogreen/index.php/"  // dev
 //"Mp0s7ZpjVLRknXNfG5qIznDLunSnBH1o71Qe0gxW2pgqm4F3a5MJtpvXNnlp5mNdoErpVZlXpBoMG3QQ9CzC14AOsWeyi1LGGd6C"
 let kSecretKey = "Mp0s7ZpjVLRknXNfG5qIznDLunSnBH1o71Qe0gxW2pgqm4F3a5MJtpvXNnlp5mNdoErpVZlXpBoMG3QQ9CzC14AOsWeyi1LGGd6C" ///manjeet Test Account
 
-
 let kAlertNoNetworkMessage = "A network connection is required. Please verify your network settings & try again."
-
 
 let kDataBaseName = "GoGreen"
 let appDelegate  = UIApplication.shared.delegate as! AppDelegate
@@ -32,7 +30,7 @@ let FORGOTPASSWORD = "api_v2/forget_password"
 let get_city = "api_v2/get_city"
 let get_locality = "api_v2/get_locality"
 let get_street = "api_v2/get_street"
-let phone_varification = "api_v2/phone_varification"
+let phone_varification = "api_v2/phone_varification"    
 let inser_cardetail = "car_packages_v2/"
 let change_password = "api_v2/"
 let my_order = "orders_api_v2"
